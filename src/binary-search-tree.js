a105399e400edd9d1e744662697d99e09d950476
undefined64 <kirillpretty@gmail.com>
2024-12-30 00:29:00 +0300

    feat: implement min&max methods of bst

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -119,13 +119,27 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+    while (node.left) {
+      node = node.left;
+    }
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+    while (node.right) {
+      node = node.right;
+    }
+    return node.data;
   }
 }
 
