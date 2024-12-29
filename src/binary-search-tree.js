13446255cc2af0d5bdcc50ae3a7e6ae1e567070d
undefined64 <kirillpretty@gmail.com>
2024-12-29 22:48:32 +0300

    feat: implement add & has & root methods of bst

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,26 +1,60 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+  constructor() {
+    this.head = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.head = addInward(this.head, data);
+
+    function addInward(node, data) {
+      if (!node) {
+        return new Node(data);
+      }
+
+      if (node.data === data) {
+        return node;
+      }
+
+      if (node.data > data) {
+        node.left = addInward(node.left, data);
+      } else {
+        node.right = addInward(node.right, data);
+      }
+
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return searchInward(this.head, data);
+
+    function searchInward(node, data) {
+      if (!node) {
+        return false;
+      }
+
+      if (node.data === data) {
+        return true;
+      }
+
+      if (data < node.data) {
+        return searchInward(node.left, data);
+      } else {
+        return searchInward(node.right, data);
+      }
+    }
   }
 
   find(/* data */) {
@@ -45,5 +79,5 @@ class BinarySearchTree {
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
